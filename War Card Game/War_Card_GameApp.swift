5e64dfb3582d655a9d4e4ce46926e0a062aba043
furqan <rahmatullah.furqan@impack-pratama.com>
2023-07-16 16:05:41 +0700

    Initial Commit

---
+++ War Card Game/War_Card_GameApp.swift
@@ -0,0 +1,17 @@
+//
+//  War_Card_GameApp.swift
+//  War Card Game
+//
+//  Created by User on 16/07/23.
+//
+
+import SwiftUI
+
+@main
+struct War_Card_GameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
